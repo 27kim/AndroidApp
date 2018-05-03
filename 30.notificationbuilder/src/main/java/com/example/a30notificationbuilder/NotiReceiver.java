2c3795c08ecdca9f66c49bc367ce0428cfe54b83
27kim <doohyeonkim@gmail.com>
2018-05-03 22:50:18 +0900

    changes applied

---
+++ 30.notificationbuilder/src/main/java/com/example/a30notificationbuilder/NotiReceiver.java
@@ -0,0 +1,15 @@
+package com.example.a30notificationbuilder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class NotiReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast toast = Toast.makeText(context, "I am Receiver", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
